9a9a5d529122ba6dffa9082722265c74831d1cbc
karthik-k4 <71215735+karthik-k4@users.noreply.github.com>
2020-10-22 21:49:59 +0530

    Update bottlesOfWaterClosure.swift
    All the changes necessary are mentioned in comments.

--- 22-10-2020/bottlesOfWaterClosure.swift
+++ 22-10-2020/bottlesOfWaterClosure.swift
@@ -1,11 +1,16 @@
 import Foundation
 
-
+//Dont leave too many blank spaces unnecessarily
 
 var bottlesOfWater = {
     (number : Int)
     in
+    //Instead of assiging number to another variable 'n', you can decrement 'number' itself. Check how to modify the input parameter of any function.
+    //What if the number was negative? You should check for error in input parameter.
+    
     var n = number
+    
+    //Leave a blank space before and after logiccal/comparison operators
     while(n>0 ){
         print("\(n) bottles of water on the wall,\n \(n) bottles of water,\n ya’ take one down, ya’ pass it around,\n \(n-1) bottles of water on the wall.")
         print("\n")
